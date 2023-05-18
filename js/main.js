de3880e03611dfae166fcc5133669c26eda51609
KaneMak <kmak159@gmail.com>
2023-05-17 21:25:31 -0500

    reformated buttons in css

--- js/main.js
+++ js/main.js
@@ -15,11 +15,17 @@ let calculator = {
 
     //make method for individual button presses 
     zero() {
-        document.querySelector("#result").innerText = 0;
+        if ( result !== 0){
+        document.querySelector("#result").innerText += 0;
+        }
     },
 
     one() {
-        document.querySelector("#result").innerText = 1;
+        if(result !== 0){
+            result: document.querySelector('#result').innerText += 1;
+        }else {
+            document.querySelector('#result').innerText += 1;
+        }
     },
 
     two() {
@@ -33,10 +39,21 @@ let calculator = {
 
 }
 
-document.querySelector(".zero").addEventListener("click", calculator.zero)
-document.querySelector(".one").addEventListener("click", calculator.one)
-document.querySelector(".two").addEventListener("click", calculator.two)
-document.querySelector(".three").addEventListener("click", calculator.three)
+
+document.querySelector(".zero").addEventListener("click", calculator.zero);
+document.querySelector(".one").addEventListener("click", calculator.one);
+document.querySelector(".two").addEventListener("click", calculator.two);
+document.querySelector(".three").addEventListener("click", calculator.three);
+
+// const numberDisplay = document.getElementById('result');
+
+// document.addEventListener('keypress', function(event) {
+//     const keyPressed = event.key;
+
+//     if (/^\d$/.test(keyPressed)) {
+//         numberDisplay.textContent += keyPressed;
+//     }
+// });
 
 // function zero() {
 //     document.querySelector("#result").innerText = 2
